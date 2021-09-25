54e6d0cd03c31edbc872e924230b04539454c8a5
yashsharma1664 <88204140+yashsharma1664@users.noreply.github.com>
2021-09-26 00:07:35 +0530

    Add files via upload

---
+++ Task 1/javascript/index.js
@@ -0,0 +1,16 @@
+$("#video_projects .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    dots: false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
